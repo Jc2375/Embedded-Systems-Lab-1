da8cb38bbd75c717787e445f49fd6d5bba941c23
Jc2375 <80490009+Jc2375@users.noreply.github.com>
2021-09-08 21:19:25 -0400

    Add files via upload

---
+++ first_zync_design/Debug/src/LED_test_tut_1C.d
@@ -0,0 +1,33 @@
+src/LED_test_tut_1C.o src/LED_test_tut_1C.o: ../src/LED_test_tut_1C.c \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xparameters.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xgpio.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_types.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_assert.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xstatus.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xgpio_l.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_io.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_printf.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xparameters.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/bspconfig.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xpseudo_asm.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xreg_cortexa9.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xpseudo_asm_gcc.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xstatus.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_printf.h
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xparameters.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xgpio.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_types.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_assert.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xstatus.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xgpio_l.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_io.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_printf.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xparameters.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/bspconfig.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xpseudo_asm.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xreg_cortexa9.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xpseudo_asm_gcc.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xstatus.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_printf.h:
---
+++ first_zync_design/Debug/src/platform.d
@@ -0,0 +1,11 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xparameters.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_cache.h \
+ /home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_types.h \
+ ../src/platform_config.h
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xparameters.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xparameters_ps.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_cache.h:
+/home/user/workspace/first_zync_system_wrapper/export/first_zync_system_wrapper/sw/first_zync_system_wrapper/standalone_ps7_cortexa9_0/bspinclude/include/xil_types.h:
+../src/platform_config.h:
